a2bfa988aa1cf047f9c8b449ee0c4c7f4d31f533
SayakaYanagi <sayanagijp@gmail.com>
2023-08-07 09:38:17 +0000

    Change logger setting

--- utils.py
+++ utils.py
@@ -7,13 +7,10 @@ import logging
 
 # Logging setting
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-fhandler = logging.FileHandler(filename=r'logs/main.log')
-formatter = logging.Formatter('%(asctime)s - %(message)s')
 
 load_dotenv()
 
-def get_default_args(user):
+def get_default_args():
 
     default_args = {
         'owner': os.environ['OWNER']
@@ -28,14 +25,14 @@ def get_api_key():
     """
 
     try:
-        logging.info('Getting exchange rate API key...')
+        logger.info('Getting exchange rate API key...')
         api_key = os.environ['EXCHANGE_RATE_API_KEY']
         return api_key
     except KeyError:
-        logging.error('Key Error in get_api_key(). Check the environmental variable EXCHANGE_RATE_API_KEY.')
+        logger.error('Key Error in get_api_key(). Check the environmental variable EXCHANGE_RATE_API_KEY.')
         raise
     except Exception as e:
-        logging.error(f'Error in get_api_key(). {type(e)} : {e}')
+        logger.error(f'Error in get_api_key(). {type(e)} : {e}')
         raise
 
 def get_rate():
@@ -47,12 +44,12 @@ def get_rate():
     req = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/GBP'
 
     try:
-        logging.info('Sending HTTP request to exchange rate API...')
+        logger.info('Sending HTTP request to exchange rate API...')
         response = requests.get(req, timeout = 10)
         jpy = response.json()['conversion_rates']['JPY']
         return jpy
     except Exception as e:
-        logging.error(f'Error in get_rate(). {type(e)} : {e}')
+        logger.error(f'Error in get_rate(). {type(e)} : {e}')
         raise
 
 def get_line_credentials():
@@ -61,15 +58,15 @@ def get_line_credentials():
     Get credentials for Line Messaging API
     """
     try:
-        logging.info('Getting LINE API credentials...')
+        logger.info('Getting LINE API credentials...')
         channel_access_token = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
         user_id = os.environ['USER_ID']
         return channel_access_token, user_id
     except KeyError:
-        logging.error('Error in get_line_credentials(). Check Line Messaging API credentials.')
+        logger.error('Error in get_line_credentials(). Check Line Messaging API credentials.')
         raise
     except Exception as e:
-        logging.error(f'Error in get_line_credentials(). {type(e) : {e}}')
+        logger.error(f'Error in get_line_credentials(). {type(e) : {e}}')
         raise
 
 def send_message():
